0b2e0e34698f9a149a64c323b86f3bded132e21e
yuchaocheng.ycc <yuchaocheng.ycc@alibaba-inc.com>
2022-12-05 09:49:13 +0800

    feat: 个人网站项目初始化

---
+++ src/layouts/BasicLayout/menuConfig.ts
@@ -0,0 +1,20 @@
+const headerMenuConfig = [
+  {
+    name: 'Dashboard',
+    path: '/',
+  },
+  {
+    name: 'Dashboard',
+    path: '/test111111111',
+  },
+];
+
+const asideMenuConfig = [
+  {
+    name: 'Dashboard',
+    path: '/',
+    icon: 'smile',
+  },
+];
+
+export { headerMenuConfig, asideMenuConfig };
---
+++ src/pages/Dashboard/components/Guide/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Button } from '@alifd/next';
+import styles from './index.module.scss';
+
+import img from '@/assets/images/bg.jpg';
+
+const Guide = () => {
+  return (
+    <div className={styles.container}>
+      {/* <img src={img} alt="" />
+      <img src={img} alt="" />
+      <img src={img} alt="" />
+      <img src={img} alt="" />
+      <img src={img} alt="" /> */}
+    </div>
+  );
+};
+
+export default Guide;
